2a1e95b0cb2c29be7ed5e2d7ac906b2443c32f31
kimchaerin <puclpu18@gmail.com>
2024-07-20 22:53:26 +0900

    🎉begin project

---
+++ src/main/java/com/mulight/dohgam/DohgamApplication.java
@@ -0,0 +1,13 @@
+package com.mulight.dohgam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DohgamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DohgamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mulight/dohgam/DohgamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mulight.dohgam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DohgamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
