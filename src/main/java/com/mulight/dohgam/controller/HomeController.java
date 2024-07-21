f91f0240e06cba719471cc4bd9dd3592d4fbf05f
kimchaerin <puclpu18@gmail.com>
2024-07-21 13:13:21 +0900

    ✨메인 화면

---
+++ src/main/java/com/mulight/dohgam/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.mulight.dohgam.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/")
+	public String home() {
+		return "home";
+	}
+	
+}
